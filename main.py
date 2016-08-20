b9556154cb18a7677681ecfada9a0857d971393d
Or Ohev-Zion <or.ohev.zion@gmail.com>
2016-08-20 13:36:11 +0300

    Write UI in PyQT ­ window with button, If you click it a message box with 'Hello World' appears.

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QMessageBox, QDesktopWidget)
+from PyQt5.QtCore import QCoreApplication
 
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    window = QtWidgets.QMainWindow()
-    button = QtWidgets.QPushButton("Hello World!")
-    window.setCentralWidget(button)
-    window.show()
-    app.exec_()
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.btn = QPushButton('Button', self)
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle('Hello World Application')
+        self.resize(250, 150)
+        self.center()
+
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(50, 50)
+        self.btn.clicked.connect(self.button_alert)
+        self.show()
+
+    def button_alert(self):
+        QMessageBox.information(self.parentWidget(),
+                                'Message', "Hello World!",
+                                QMessageBox.Ok)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
